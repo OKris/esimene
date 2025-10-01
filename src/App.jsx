03729eabe726205f631c4b658ffcfadb5dbe454c
OKris <oim.kristina@gmail.com>
2025-10-01 19:39:12 +0300

    lisatud pilt

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Luuletus from './Luuletus'
+import Pilt from './assets/pilt1.jpg'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
     <>
     <h1>Poeem</h1>
     <Luuletus />
+    <img className='esipilt' src={Pilt} />
     </>
   )
 }
